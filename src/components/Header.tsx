52e84121ee48bc60b34155009df92b1a55b57ca6
hoangnt391 <hoangnt391@gmail.com>
2023-06-30 10:08:58 +0700

    add header footer render data

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <header>
+      <img className="w-[80px]" src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Netflix_logo.png" alt="" />
+    </header>
+  )
+}
+
+export default Header
